d08482b949eca7f2b1838456ba5e84d37d34a138
Tyler Shannon <tjshanno@ncsu.edu>
2020-11-15 13:27:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+var idleTime = 0;
+$(document).ready(function () {
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 1000); // 1 seconds
+
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+        document.getElementById("still-there").classList.add("hidden");
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+    $(this).click(function (e) {
+        idleTime = 0;
+    });
+
+});
+
+$('#courtforms').on('click', function(e) {
+    //e.preventDefault();
+    alert('test');
+  });
+
+function logBackIn() {
+  document.getElementById("refresh").classList.add("hidden");
+}
+function newSession() {
+  document.getElementById("refresh").classList.add("hidden");
+}
+
+
+function timerIncrement() {
+    //console.log(idleTime);
+    idleTime = idleTime + 1;
+    if (idleTime > 20) { // 20 minutes
+        //window.location.reload();
+        document.getElementById("still-there").classList.add("hidden");
+        document.getElementById("refresh").classList.remove("hidden");
+    }
+    if (idleTime > 10 & idleTime < 21) {
+        //window.location.reload();
+        document.getElementById("still-there").classList.remove("hidden");
+        var remaining = 20-idleTime;
+        document.getElementById("countdown").innerHTML = `Your session will log out in ${remaining} seconds`
+    }
+}
+
+function resetTimer() {
+  idleTime = 0
+}
